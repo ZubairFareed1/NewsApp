d5619fa0c620546669169cf7bd7591a061e44c27
zubair ahmed <zubairahmedjamali908@gmail.com>
2024-01-01 21:53:48 +0500

    Lecture 23 add navbar and a news class component and news item class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className='App'>
+      <Navbar />
+      <News />
+      </div>
     )
   }
 }
